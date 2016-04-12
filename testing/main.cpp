f29a1917dd318e34be67fa126021e0295bdacf6e
ismann <ismann@protonmail.com>
2016-04-12 12:40:14 -0400

    Add testing folder for small scripts before implementation

---
+++ testing/main.cpp
@@ -0,0 +1,65 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QString>
+#include <QStringList>
+#include <QTcpSocket>
+#include <QTextStream>
+#include <QDebug>
+#include <string>
+
+QTcpSocket socket;
+
+int main() {
+   // File for text
+   QFile *outFile = new QFile("out.txt");
+   QTextStream outStream(outFile);
+   
+   if (!outFile->open(QFile::WriteOnly | QFile::Text)) {
+      qDebug() << "Could not open file for writing.\n";
+      return 1;
+   }
+   
+   // Data
+   QString host = "chat.freenode.net";
+   int port = 6667;
+      
+   socket.connectToHost(host, port);
+   if (socket.waitForConnected(3000)) {
+      socket.write("NICK user1234\r\n");
+      socket.write("USER user1234 0 * :user1234\r\n");
+      socket.write("JOIN ##math");
+      socket.waitForBytesWritten();
+      socket.waitForReadyRead();
+      
+      qDebug() << "Reading: " << socket.bytesAvailable() << " bytes.";
+      qDebug() << socket.readAll();
+      
+      std::string serverResp;
+      for (;;) {
+         socket.waitForReadyRead(100);
+         serverResp = socket.readAll().toStdString();
+         
+         if (!serverResp.empty()) {
+            qDebug() << serverResp.c_str();
+            outStream << serverResp.c_str();
+            
+            size_t spaceIdx = serverResp.find(' ');
+            if (serverResp.substr(0, spaceIdx).compare("PING") == 0) {
+               std::string pongStr = "PONG" + serverResp.substr(spaceIdx);
+               socket.write(pongStr.c_str());
+               qDebug() << "PONG sent.";
+            }
+         }
+      }
+      
+      outFile->flush();
+      outFile->close();
+      delete outFile;
+      socket.close();
+   }
+   else {
+      qDebug() << "Not connected...";
+   }
+   
+   return 0;
+}
