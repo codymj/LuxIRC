f2403747a02a75ad7e6c18c50a089c1a7623836e
Cody Johnson <codyj@protonmail.com>
2016-07-25 15:45:13 -0400

    Create Connection class to handle connection logic

---
+++ src/Connection.h
@@ -0,0 +1,20 @@
+/******************************************************************************
+ * Connection.h                                                               *
+ *                                                                            *
+ * Created on Jul 25, 2016                                                    *
+ * Author: Cody Johnson <codyj@protonmail.com>                                *
+ ******************************************************************************/
+
+#ifndef _CONNECTION_H_
+#define _CONNECTION_H_
+
+class Connection {
+public:
+
+
+private:
+
+
+};
+
+#endif // _CONNECTION_H_
